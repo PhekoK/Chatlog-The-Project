81fddbeed25a17fbe8982c85879c0ced75d00c07
Karabo Pheko <216021429@student.uj.ac.za>
2021-07-16 15:09:15 +0200

    Third Commit

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var path = require('path');
 
 var http = require('http').Server(app);
 
-//app.use(express.static(path.join(__dirname, '/registration')));
-app.use(express.static(__dirname));
-
+app.use(express.static(path.join(__dirname, 'public')));
+/* app.use(express.static(__dirname));
+ */
 
 http.listen(3000, () => {
     console.log('Server is running at port 3000!!');
