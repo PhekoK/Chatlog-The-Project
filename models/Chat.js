b1674ddaf9aa2915511f3e74921a3373f558192e
Karabo Pheko <216021429@student.uj.ac.za>
2021-07-17 01:13:04 +0200

    More Commits

--- models/Chat.js
+++ models/Chat.js
@@ -1,11 +1,15 @@
 var mongoose = require('mongoose');
 
 var chatSchema = new mongoose.Schema({
-    user: [ 
-        { type: mongoose.Schema.ObjectId, ref: 'User' }
-    ], //name of user who sent you the message
-    msg: { type: String, required: true}, //
-    created_at: { type: Date, default: Date.now}
-}, { versionKey: false});
+    user: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
+    msg: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+}, { versionKey: false });
 
 module.exports = mongoose.model('Chat', chatSchema);
